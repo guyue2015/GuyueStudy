eea6681ac56ff1dab27f68fdeaa38bbc27425783
胡贺东 <1182738042@qq.com>
2017-01-09 13:00:06 +0800

    增加通用springmvc以及springboot项目

---
+++ CommonProject/20170109SpringBoot/src/main/java/com/guyue/ProjectMangerSpringBoot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.guyue.ProjectMangerSpringBoot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	String index() {
+		return "Hello World!";
+	}
+}
