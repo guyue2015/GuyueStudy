71cd394b1f58e2093bcee8256eb4342d091b0134
HUHEDONG\Administrator <1182738042@qq.com>
2017-04-17 13:28:13 +0800

    增加倪雪分裂数据内容

---
+++ java/SpringBoot/src/main/java/com/guyue/springboot/testNxTaobao/TaobaoUtil.java
@@ -0,0 +1,18 @@
+package com.guyue.springboot.testNxTaobao;
+
+public class TaobaoUtil {
+
+	/**
+	 * 
+	 * @param titleFileDirectory 标题文件目录
+	 * @param titleFile 标题文件
+	 * @param taobaoHelpFile 淘宝助手文件
+	 * @param resultFilePath 生成文件目录
+	 * @return 生成文件路径
+	 */
+	public static String createTaobaoFile(String titleFileDirectory,
+			String titleFile, String taobaoHelpFile, String resultFilePath) {
+		return null;
+	}
+
+}
---
+++ java/SpringBoot/src/main/java/com/guyue/springboot/testNxTaobao/TestCreateTabaoData.java
@@ -0,0 +1,64 @@
+package com.guyue.springboot.testNxTaobao;
+
+import org.apache.commons.io.FileUtils;
+
+import com.guyue.common.util.FileUtil;
+
+/**
+ * 测试淘宝数据合成，也就是倪雪说的数据分裂
+ * @author Administrator
+ *
+ */
+public class TestCreateTabaoData {
+	/**
+	 * 标题栏目录
+	 */
+	public static String titleFileDirectory="D:/迅雷下载/倪雪淘宝/小奈、4.5/标题栏/";
+	/**
+	 * 标题栏文件目录
+	 */
+	public static String titleFile="D:/迅雷下载/倪雪淘宝/小奈、4.5/NX.xls";
+	/**
+	 * 淘宝助理文件目录
+	 */
+	public static String taobaoHelpFile="D:/迅雷下载/倪雪淘宝/小奈、4.5/淘宝助理5201704042156.csv";
+	
+	/**
+	 * 生成文件目录
+	 */
+	public static String resultFilePath ="D:/迅雷下载/倪雪淘宝/小奈、4.5/";
+	
+	public static void main(String[] args) {
+		if(args!=null&&args.length==4){
+			titleFileDirectory = args[0];
+			titleFile = args[1];
+			taobaoHelpFile = args[2];
+			resultFilePath = args[3];
+		}
+		if(!titleFileDirectory.endsWith("/")){
+			titleFileDirectory=titleFileDirectory+"/";
+		}
+		if(!resultFilePath.endsWith("/")){
+			resultFilePath=resultFilePath+"/";
+		}
+		if(!FileUtil.exists(titleFileDirectory)){
+			System.out.println("文件目录不存在:"+titleFileDirectory);
+			return;
+		}
+		if(!FileUtil.exists(titleFile)){
+			System.out.println("文件目录不存在:"+titleFile);
+			return;
+		}
+		if(!FileUtil.exists(taobaoHelpFile)){
+			System.out.println("文件目录不存在:"+taobaoHelpFile);
+			return;
+		}
+		if(!FileUtil.exists(resultFilePath)){
+			System.out.println("文件目录不存在:"+resultFilePath);
+			return;
+		}
+		
+		String resultFile = TaobaoUtil.createTaobaoFile(titleFileDirectory,titleFile,taobaoHelpFile,resultFilePath);
+		System.out.println("文件已生成，生成文件位于:"+resultFile);
+	}
+}
